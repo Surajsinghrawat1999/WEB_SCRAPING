e4453f58a1cc4873f7ce6eac65dd989c2bdeef09
Suraj Rawat <surajsinghrawat1999@gmail.com>
2023-07-26 19:18:51 +0530

    Day 38 - 2 video Extract the tour details from web page

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ import requests
 import selectorlib
 
 URL = "http://programmer100.pythonanywhere.com/tours/"
-
-
-HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko)'
-                         ' Chrome/39.0.2171.95 Safari/537.36'}
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/39.0.2171.95 Safari/537.36'}
 
 
 def scrap(url):
@@ -14,5 +13,13 @@ def scrap(url):
     return source
 
 
+def extract(source):
+    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+    value = extractor.extract(source)["tours"]
+    return value
+
+
 if __name__ == "__main__":
-    print(scrap(URL))
+    scraped = scrap(URL)
+    extracted = extract(scraped)
+    print(extracted)
